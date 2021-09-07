08aa2291a52aa2b5440b9df4b275fda839c4b325
Carlil Jean <jaedagk@live.com>
2021-09-07 04:08:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+loveCount = name1.lower() + name2.lower()
+trueCount = name1.lower() + name2.lower()
+
+
