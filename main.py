3245e99f789de7f7d3fa8a04a9722a74a29915cf
Carlil Jean <jaedagk@live.com>
2021-09-07 08:32:16 +0000

    Final commit.

--- main.py
+++ main.py
@@ -7,5 +7,12 @@ name2 = input("What is their name? \n")
 #Write your code below this line 👇
 loveCount = name1.lower() + name2.lower()
 trueCount = name1.lower() + name2.lower()
-
-
+loveCompatibility = loveCount.count("l") + loveCount.count("o") + loveCount.count("v") + loveCount.count("e") 
+trueCompatibility = trueCount.count("t") + trueCount.count("o") + loveCount.count("v") + loveCount.count("e")
+combinedCompatibility = int(str(loveCompatibility) + str(trueCompatibility))
+if combinedCompatibility < 10 or combinedCompatibility > 90:
+  print("Your score is {}, you go together like coke and mentos.".format(combinedCompatibility))
+elif 40 <= combinedCompatibility >= 50:
+  print("Your score is {}, you are alright together.".format(combinedCompatibility))
+else:
+  print("Your score is {}".format(combinedCompatibility))
